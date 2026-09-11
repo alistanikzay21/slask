9e9e29a9ff869d7c4004ba59dca62abf2f26c356
Ali Kawsar <alikawsar2005@gmail.com>
2026-09-11 13:27:01 +0200

    Nisse was here!

--- obj/Debug/net10.0/simpleGit.AssemblyInfo.cs
+++ obj/Debug/net10.0/simpleGit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("simpleGit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac05bb55b939c1c2f925e4d921eee1a5ff564681")]
 [assembly: System.Reflection.AssemblyProductAttribute("simpleGit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("simpleGit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
